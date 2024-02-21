9f1073e3f19d29417f0a4f1822c4a35db696ae23
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-02-21 13:39:01 +0700

    ParticalView and form

--- V2/Controllers/HomeController.cs
+++ V2/Controllers/HomeController.cs
@@ -6,21 +6,20 @@ namespace V2.Controllers
 {
     public class HomeController : Controller
     {
-       
-        Group _group = new Group(1,"e11",new List<int>() { 1,2,3});
-        Student _student_1 = new Student(1,"Ivan",1);
-        Student _student_2 = new Student(2,"Jon",1);
-        Student _student_3 = new Student(3,"Piter",1);
+
+        Group _group = new Group(1, "e11", new List<int>() { 1, 2, 3 });
+        Student _student_1 = new Student(1, "Ivan", 1);
+        Student _student_2 = new Student(2, "Jon", 1);
+        Student _student_3 = new Student(3, "Piter", 1);
 
         public IActionResult Index()
         {
-            IndexViewModel _indexViewModel = new IndexViewModel(
-                _group, 
-                _student_1
-                );
-            ViewBag.name = "Ivan";
-            ViewData["name"] = "Ivan";
-            return View(_indexViewModel); 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string studentName, string groupName)
+        {
+            return View(new List<string>() { studentName, groupName});
         }
         public IActionResult Short()
         {
